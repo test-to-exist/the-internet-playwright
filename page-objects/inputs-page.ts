fe12ad533e0c7948fcc01a23e9608e469b833d55
tpichlak <tpichlak@gmail.com>
2022-03-10 21:57:57 +0100

    working on the inputs page tests.

---
+++ page-objects/inputs-page.ts
@@ -0,0 +1,15 @@
+import { Locator, Page } from "@playwright/test";
+
+export class InputsPage {
+  readonly page: Page;
+  readonly numberInput: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.numberInput = page.locator("input[type='number']");
+  }
+
+  async fillInput(value: string) {
+    this.numberInput.type(value);
+  }
+}
--- page-objects/main-page.ts
+++ page-objects/main-page.ts
@@ -4,11 +4,14 @@ export class MainPage {
   readonly page: Page;
   readonly abTestLink: Locator;
   readonly addRemoveElementsLink: Locator;
+  readonly inputsLink: Locator;
+
 
   constructor(page: Page) {
     this.page = page;
     this.abTestLink = page.locator("a[href='/abtest']");
     this.addRemoveElementsLink = page.locator("a[href='/add_remove_elements/']");
+    this.inputsLink = page.locator("a[href='/inputs']").first();
   }
 
   async abTest() {
@@ -17,5 +20,9 @@ export class MainPage {
 
   async addRemoveElements() {
     await this.addRemoveElementsLink.click();
+  }
+
+  async inputs() {
+    await this.inputsLink.click();
   }
 }
