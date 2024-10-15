d567064becc0bf10db8b8073d31116c05b1db098
tomaszpichlak <tomasz@techtailor.co>
2024-10-15 14:05:20 +0200

    add fixture for the floating menu page

--- tests/floating-menu.spec.ts
+++ tests/floating-menu.spec.ts
@@ -1,16 +1,21 @@
-import { expect, test } from '@playwright/test';
+import { FloatingMenuPage } from '@pages/floating-menu-page';
+import { MainPage } from '@pages/main-page';
+import { expect, test as base } from '@playwright/test';
 
-test('After scrolling down the floating menu is still visible', async ({ page }) => {
-  await page.goto(`${process.env.BASE_URL}/floating_menu`);
-  const homeLink = page.getByRole('link', { name: 'Home' });
-  const newsLink = page.getByRole('link', { name: 'News' });
-  const contactLink = page.getByRole('link', { name: 'Contact' });
-  const aboutLink = page.getByRole('link', { name: 'About' });
+const test = base.extend<{ floatingMenuPage: FloatingMenuPage }>({
+  floatingMenuPage: async ({ page }, use) => {
+    await page.goto(process.env.BASE_URL);
+    const mainPage = new MainPage(page);
+    const floatingMenuPage = await mainPage.floatingMenu();
+    use(floatingMenuPage);
+  },
+});
 
-  await expect(homeLink).toBeVisible();
-  await expect(newsLink).toBeVisible();
-  await expect(contactLink).toBeVisible();
-  await expect(aboutLink).toBeVisible();
+test('After scrolling down the floating menu is still visible', async ({ floatingMenuPage, page }) => {
+  await expect(floatingMenuPage.homeLink).toBeVisible();
+  await expect(floatingMenuPage.newsLink).toBeVisible();
+  await expect(floatingMenuPage.contactLink).toBeVisible();
+  await expect(floatingMenuPage.aboutLink).toBeVisible();
 
   const scrollHeight = await page.evaluate(() => {
     return window.document.body.scrollHeight;
@@ -18,8 +23,8 @@ test('After scrolling down the floating menu is still visible', async ({ page })
 
   await page.mouse.wheel(0, scrollHeight);
 
-  await expect(homeLink).toBeVisible();
-  await expect(newsLink).toBeVisible();
-  await expect(contactLink).toBeVisible();
-  await expect(aboutLink).toBeVisible();
+  await expect(floatingMenuPage.homeLink).toBeVisible();
+  await expect(floatingMenuPage.newsLink).toBeVisible();
+  await expect(floatingMenuPage.contactLink).toBeVisible();
+  await expect(floatingMenuPage.aboutLink).toBeVisible();
 });
