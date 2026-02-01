421c30afe69c722ffad06905e9352e9839cf4ffa
test-to-exist <test-to-exist@users.noreply.github.com>
2026-02-01 17:04:59 +0000

    deploy: 43db8de80da99ac0c44fedbea7472980416ba2b7

--- tests/exit-intent.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test,  expect } from "@playwright/test";
-
-
-test('The modal should shown on mouse leaving the viewport', async ({ page }) => {
-  await page.goto(`${process.env.BASE_URL}/exit_intent`);
-  await page.locator('html').dispatchEvent('mouseleave');
-  await expect(page.getByText(
-    "It's commonly used to encourage a user to take an action (e.g., give their e-mail address to sign up for something).")).toBeVisible();
-});
