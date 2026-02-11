90a276fc4a23e2a1ebeb731d11da5903de0947f0
Tomasz Pichlak <testtoexist@fastmail.com>
2026-02-11 19:44:34 +0100

    Trying to fix the test but getting random failures.

--- tests/file-downloader.spec.ts
+++ tests/file-downloader.spec.ts
@@ -1,13 +1,29 @@
 import { test,  expect } from "@playwright/test";
 import * as fs from 'fs'
+import * as appRootPath from 'app-root-path';
+import * as path from 'path';
+import { v4 } from 'uuid';
+
+let filePath: string;
+
+
+test.afterEach(() => {
+  fs.unlinkSync(filePath);
+})
 
 test('User should be able to download a file', async ({ page }) => {
   await page.goto(`${process.env.BASE_URL}/download`);
   const downloadPromise = page.waitForEvent('download');
-  await page.getByRole('link', {name: 'zero_bytes_file.txt' , exact: true}).click(); 
+  await page.getByRole('link', {name: 'some-file.txt' , exact: true}).click(); 
   const download = await downloadPromise;
-  const filePath = '../downloads/' + download.suggestedFilename()
+  const downloadDir = path.join(appRootPath.path, 'downloads');
+
+  if (!fs.existsSync(downloadDir)) {
+    fs.mkdirSync(downloadDir, { recursive: true });
+  }
+  filePath = path.join(downloadDir, v4() + '.txt');
   await download.saveAs(filePath);
   
+  await page.waitForTimeout(5000);
   expect(fs.existsSync(filePath)).toBeTruthy();
 });
