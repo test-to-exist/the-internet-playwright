907d0272dbccf913a95d8e2eefbfa7f05b773f91
Tomasz Pichlak <testtoexist@fastmail.com>
2026-02-11 20:01:53 +0100

    Add describe block

--- tests/file-downloader.spec.ts
+++ tests/file-downloader.spec.ts
@@ -4,26 +4,27 @@ import * as appRootPath from 'app-root-path';
 import * as path from 'path';
 import * as uuid from 'uuid';
 
-let filePath: string;
+test.describe('File Downloader Tests', () => {
+  let filePath: string;
 
+  test.afterEach(() => {
+    fs.unlinkSync(filePath);
+  })
 
-test.afterEach(() => {
-  fs.unlinkSync(filePath);
-})
+  test('User should be able to download a file', async ({ page }) => {
+    await page.goto(`${process.env.BASE_URL}/download`);
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('link', {name: 'some-file.txt' , exact: true}).click(); 
+    const download = await downloadPromise;
+    const downloadDir = path.join(appRootPath.path, 'downloads');
 
-test('User should be able to download a file', async ({ page }) => {
-  await page.goto(`${process.env.BASE_URL}/download`);
-  const downloadPromise = page.waitForEvent('download');
-  await page.getByRole('link', {name: 'some-file.txt' , exact: true}).click(); 
-  const download = await downloadPromise;
-  const downloadDir = path.join(appRootPath.path, 'downloads');
-
-  if (!fs.existsSync(downloadDir)) {
-    fs.mkdirSync(downloadDir, { recursive: true });
-  }
-  filePath = path.join(downloadDir, uuid.v4() + '.txt');
-  await download.saveAs(filePath);
-  
-  await page.waitForTimeout(5000);
-  expect(fs.existsSync(filePath)).toBeTruthy();
+    if (!fs.existsSync(downloadDir)) {
+      fs.mkdirSync(downloadDir, { recursive: true });
+    }
+    filePath = path.join(downloadDir, uuid.v4() + '.txt');
+    await download.saveAs(filePath);
+    
+    await page.waitForTimeout(5000);
+    expect(fs.existsSync(filePath)).toBeTruthy();
+  });
 });
