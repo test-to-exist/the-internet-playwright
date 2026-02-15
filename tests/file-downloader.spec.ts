5a9c903ca4359df65dbfef20c88c663849764d0f
Tomasz Pichlak <testtoexist@fastmail.com>
2026-02-15 18:30:42 +0100

    Stupid timeouts seems to fix the issue

--- tests/file-downloader.spec.ts
+++ tests/file-downloader.spec.ts
@@ -1,6 +1,5 @@
 import { test,  expect } from "@playwright/test";
 import * as fs from 'fs'
-import * as appRootPath from 'app-root-path';
 import * as path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -16,15 +15,13 @@ test.describe('File Downloader Tests', () => {
     const downloadPromise = page.waitForEvent('download');
     await page.getByRole('link', {name: 'some-file.txt' , exact: true}).click(); 
     const download = await downloadPromise;
-    const downloadDir = path.join(appRootPath.path, 'downloads');
+    const downloadDir = 'downloads';
 
-    if (!fs.existsSync(downloadDir)) {
-      fs.mkdirSync(downloadDir, { recursive: true });
-    }
     filePath = path.join(downloadDir, uuidv4() + '.txt');
+    await page.waitForTimeout(10_000);
     await download.saveAs(filePath);
-    
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(5_000);
+
     expect(fs.existsSync(filePath)).toBeTruthy();
   });
 });
